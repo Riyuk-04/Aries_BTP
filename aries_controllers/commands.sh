c30c5e09613bd49d1d35e49640c4490418eb8a86
Riyuk-04 <ishangoel04062001@gmail.com>
2023-04-02 16:36:21 +0530

    creds-issued

--- aries_controllers/commands.sh
+++ aries_controllers/commands.sh
@@ -1,12 +1,12 @@
 sudo docker run -itd -p 9701-9708:9701-9708 ghoshbishakh/indy_pool
 
 aca-py start -it http 0.0.0.0 8000 -ot http -e http://127.0.0.1:8000 --admin-insecure-mode --admin 0.0.0.0 8001 \
---genesis-file /home/ishan/pool1.txn --seed 000000000000000000000000Steward1 --wallet-type indy --wallet-name agent1 \
---wallet-key agent1 --log-level debug --replace-public-did --auto-provision
+--genesis-file /home/ishan/pool1.txn --seed 000000000000000000000000Steward1 --wallet-type indy --wallet-name q15 \
+--wallet-key q15 --log-level debug --replace-public-did --auto-provision --auto-store-credential --auto-accept-requests
 
 aca-py start -it http 0.0.0.0 9000 -ot http -e http://127.0.0.1:9000 --admin-insecure-mode --admin 0.0.0.0 9001 \
---genesis-file /home/ishan/pool1.txn --wallet-type indy --wallet-name agent2 --wallet-key agent2 \
---log-level debug --replace-public-did --auto-provision
+--genesis-file /home/ishan/pool1.txn --wallet-type indy --wallet-name q16 --wallet-key q16 \
+--log-level debug --replace-public-did --auto-provision --auto-store-credential --auto-accept-requests
 
 aca-py provision --wallet-type indy --seed 000000000000000000000000Steward1 -e 127.0.0.1:8000 --genesis-file /home/ishan/pool1.txn \
 --wallet-name agent1 --wallet-key agent1
@@ -14,77 +14,3 @@ aca-py provision --wallet-type indy --seed 000000000000000000000000Steward1 -e 1
 aca-py provision --wallet-type indy -e http://127.0.0.1:9000 --genesis-file /home/ishan/pool1.txn --wallet-name steward1 --wallet-key steward1
 
 --public-invites --auto-ping-connection --auto-accept-invites --auto-accept-requests --auto-respond-presentation-proposal --auto-store-credential --auto-verify-presentation
-
-{
-  "auto_remove": true,
-  "comment": "string",
-  "cred_def_id": "Th7MpTaRZVRYnPiabds81Y:3:CL:16:default",
-  "credential_proposal": {
-    "@type": "issue-credential/1.0/credential-preview",
-    "attributes": [
-      {
-        "weight": "70"
-      }
-    ]
-  },
-  "issuer_did": "Th7MpTaRZVRYnPiabds81Y",
-  "schema_id": "Th7MpTaRZVRYnPiabds81Y:2:body_weight:1.0",
-  "schema_issuer_did": "Th7MpTaRZVRYnPiabds81Y",
-  "schema_name": "body_weight",
-  "schema_version": "1.0",
-  "trace": true
-}
-
-{
-  "auto_remove": true,
-  "comment": "string",
-  "connection_id": "d1d273d9-f106-4ab0-b6d8-7fe8d1fdda7e",
-  "credential_preview": {
-    "@type": "issue-credential/2.0/credential-preview",
-    "attributes": [
-      {
-        "score": "90"
-      }
-    ]
-  },
-  "filter": {
-    "indy": {
-      "cred_def_id": "Th7MpTaRZVRYnPiabds81Y:3:CL:14:default",
-      "issuer_did": "Th7MpTaRZVRYnPiabds81Y",
-      "schema_id": "Th7MpTaRZVRYnPiabds81Y:2:prefs:1.0",
-      "schema_issuer_did": "Th7MpTaRZVRYnPiabds81Y",
-      "schema_name": "prefs",
-      "schema_version": "1.0"
-    },
-    "ld_proof": {
-      "credential": {
-        "@context": [
-          "https://www.w3.org/2018/credentials/v1",
-          "https://w3id.org/citizenship/v1"
-        ],
-        "credentialSubject": {
-          "familyName": "SMITH",
-          "gender": "Male",
-          "givenName": "JOHN",
-          "type": [
-            "PermanentResident",
-            "Person"
-          ]
-        },
-        "description": "Government of Example Permanent Resident Card.",
-        "identifier": "83627465",
-        "issuanceDate": "2019-12-03T12:19:52Z",
-        "issuer": "did:key:z6MkmjY8GnV5i9YTDtPETC2uUAW6ejw3nk5mXF5yci5ab7th",
-        "name": "Permanent Resident Card",
-        "type": [
-          "VerifiableCredential",
-          "PermanentResidentCard"
-        ]
-      },
-      "options": {
-        "proofType": "Ed25519Signature2018"
-      }
-    }
-  },
-  "trace": true
-}
--- aries_controllers/utils.py
+++ aries_controllers/utils.py
@@ -7,7 +7,7 @@ import json
     ##Creates and post public DID
     ##SAMPLE RESP : {'result': {'did': 'Hc4GRdjapBrCZf83ypqJGW', 'verkey': 'A3u7HykmMQdv1vsBqiTdMbZVdf6u2Sos4nNuMafMS5Kn', 'posture': 'posted', 'key_type': 'ed25519', 'method': 'sov'}}
 def create_public_did(agent1_admin_port, steward_admin_port):
-    url_create_did = "http://0.0.0.0:" + str(agent1_admin_port) + "/wallet/did/create"
+    url_create_did = "http://127.0.0.1:" + str(agent1_admin_port) + "/wallet/did/create"
     headers_create_did = {"accept": "application/json", "Content-Type": "application/json"}
     payload_create_did = {
         "method": "sov",
@@ -19,129 +19,220 @@ def create_public_did(agent1_admin_port, steward_admin_port):
     try:
         r_create_did = requests.post(url=url_create_did, json=payload_create_did, headers=headers_create_did)
         r_create_did = r_create_did.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
     
     did  = r_create_did['result']['did']
     verkey = r_create_did['result']['verkey']
 
-    url_reg_nym = "http://0.0.0.0:" + str(steward_admin_port) + "/ledger/register-nym?did=" + str(did) + "&verkey=" + str(verkey)
+    url_reg_nym = "http://127.0.0.1:" + str(steward_admin_port) + "/ledger/register-nym?did=" + str(did) + "&verkey=" + str(verkey)
     headers_reg_nym = {"accept": "application/json"}
     payload_reg_nym = {}
 
     try:
         r_reg_nym = requests.post(url=url_reg_nym, json=payload_reg_nym, headers=headers_reg_nym)
         r_reg_nym = r_reg_nym.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
     
-    url_post_did = "http://0.0.0.0:" + str(agent1_admin_port) + "/wallet/did/public?did=" + str(did)
+    url_post_did = "http://127.0.0.1:" + str(agent1_admin_port) + "/wallet/did/public?did=" + str(did)
     headers_post_did = {"accept": "application/json"}
     payload_post_did = {}
 
     try:
         r_post_did = requests.post(url=url_post_did, json=payload_post_did, headers=headers_post_did)
         r_post_did = r_post_did.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
     
     return r_post_did
 
 def make_Connection(agent1_admin_port, steward_admin_port):
-    url_create_inv = "http://0.0.0.0:" + str(steward_admin_port) + "/connections/create-invitation"
+    url_create_inv = "http://127.0.0.1:" + str(steward_admin_port) + "/connections/create-invitation"
     headers_create_inv = {"accept": "application/json", "Content-Type": "application/json"}
     payload_create_inv = {}
 
     try:
         r_create_inv = requests.post(url=url_create_inv, json=payload_create_inv, headers=headers_create_inv)
         r_create_inv = r_create_inv.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
 
     invitation = r_create_inv['invitation']
     steward_conn_id = r_create_inv['connection_id']
 
-    url_rcv_inv = "http://0.0.0.0:" + str(agent1_admin_port) + "/connections/receive-invitation"
+    url_rcv_inv = "http://127.0.0.1:" + str(agent1_admin_port) + "/connections/receive-invitation"
     headers_rcv_inv = {"accept": "application/json", "Content-Type": "application/json"}
     payload_rcv_inv = invitation
 
     try:
         r_rcv_inv = requests.post(url=url_rcv_inv, json=payload_rcv_inv, headers=headers_rcv_inv)
         r_rcv_inv = r_rcv_inv.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
     
     alice_conn_id = r_rcv_inv['connection_id']
-    print(steward_conn_id, alice_conn_id)
 
-
-    url_accept_inv = "http://0.0.0.0:" + str(agent1_admin_port) + "/connections/" + str(alice_conn_id) + "/accept-invitation"
+    url_accept_inv = "http://127.0.0.1:" + str(agent1_admin_port) + "/connections/" + str(alice_conn_id) + "/accept-invitation"
     headers_accept_inv = {"accept": "application/json"}
     payload_accept_inv = {}
 
     try:
         r_accept_inv = requests.post(url=url_accept_inv, json=payload_accept_inv, headers=headers_accept_inv)
         r_accept_inv = r_accept_inv.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
 
 
     time.sleep(0.1)
-    url_accept_req = "http://0.0.0.0:" + str(steward_admin_port) + "/connections/" + str(steward_conn_id) + "/accept-request"
+    url_accept_req = "http://127.0.0.1:" + str(steward_admin_port) + "/connections/" + str(steward_conn_id) + "/accept-request"
     headers_accept_req = {"accept": "application/json"}
     payload_accept_req = {}
 
     try:
         r_accept_req = requests.post(url=url_accept_req, data=payload_accept_req, headers=headers_accept_req)
         r_accept_req = r_accept_req.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
     
     return steward_conn_id, alice_conn_id
 
 def gen_Schema(agent_admin_port, schema):
-    url_schema = "http://0.0.0.0:" + str(agent_admin_port) + "/schemas"
+    url_schema = "http://127.0.0.1:" + str(agent_admin_port) + "/schemas"
     headers_schema = {"accept": "application/json", "Content-Type": "application/json"}
     payload_schema = str(schema)
 
     try:
         r_schema = requests.post(url=url_schema, data=payload_schema, headers=headers_schema)
         r_schema = r_schema.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+    except Exception as errh:
+        print(errh)
 
     return r_schema
 
 def gen_CredDef(agent_admin_port, credDef):
-    url_CredDef = "http://0.0.0.0:" + str(agent_admin_port) + "/credential-definitions"
+    print(credDef)
+    url_CredDef = "http://127.0.0.1:" + str(agent_admin_port) + "/credential-definitions"
     headers_CredDef = {"accept": "application/json", "Content-Type": "application/json"}
     payload_CredDef = credDef
     try:
         r_CredDef = requests.post(url=url_CredDef, data=payload_CredDef, headers=headers_CredDef)
-        r_CredDef = r_CredDef.json()
-    except requests.exceptions.HTTPError as errh:
-        raise errh
+        if r_CredDef.status_code == 200:
+            r_CredDef = r_CredDef.json()
+        else:
+            print(r_CredDef.text)
+    except Exception as errh:
+        print(errh)
 
     return r_CredDef
 
-if __name__ == "__main__":
-    # print(create_public_did(9001, 8001))
-    # print(make_Connection(9001, 8001))
-    sample_schema = {
-  "attributes": [
-    "score"
-  ],
-  "schema_name": "prefs",
-  "schema_version": "1.0"
-}
-    json_schema = json.dumps(sample_schema)
-    # print(gen_Schema(8001, json_schema))
+def issue_Cred(steward_admin_port, agent_admin_port, cred):
+    url_send_offer = "http://127.0.0.1:" + str(steward_admin_port) + "/issue-credential-2.0/send"
+    headers_send_offer = {"accept": "application/json", "Content-Type": "application/json"}
+    payload_send_offer = cred
+    try:
+        r_send_offer = requests.post(url=url_send_offer, data=payload_send_offer, headers=headers_send_offer)
+        if r_send_offer.status_code == 200:
+            r_send_offer = r_send_offer.json()
+        else:
+            print(r_send_offer.text)
+    except Exception as errh:
+        print(errh)
 
-    sample_CredDef = {
-  "revocation_registry_size": 1000,
-  "schema_id": "Th7MpTaRZVRYnPiabds81Y:2:prefs2:1.0",
-  "support_revocation": True,
-  "tag": "default"
-}
-    json_CredDef = json.dumps(sample_CredDef)
-    print(gen_CredDef(8001, json_CredDef))
+    return r_send_offer
+
+def send_cred_req(agent_admin_port, agent_did):
+    url_fetch_cred_ex = "http://127.0.0.1:" + str(agent_admin_port) + "/issue-credential-2.0/records"
+    headers_fetch_cred_ex = {"accept": "application/json"}
+    payload_fetch_cred_ex = {}
+    try:
+        r_fetch_cred_ex = requests.get(url=url_fetch_cred_ex, data=payload_fetch_cred_ex, headers=headers_fetch_cred_ex)
+        if r_fetch_cred_ex.status_code == 200:
+            r_fetch_cred_ex = r_fetch_cred_ex.json()
+        else:
+            print(r_fetch_cred_ex.text)
+    except Exception as errh:
+        print(errh)
+    
+    cred_ex_id = r_fetch_cred_ex['results'][0]['cred_ex_record']['cred_ex_id']
+    
+    url_send_req = "http://127.0.0.1:" + str(agent_admin_port) + "/issue-credential-2.0/records/" + str(cred_ex_id) + "/send-request"
+    headers_send_req = {"accept": "application/json", "Content-Type": "application/json"}
+    payload_send_req_t = {
+        "holder_did": agent_did
+    }
+    payload_send_req = json.dumps(payload_send_req_t)
+    try:
+        r_send_req = requests.post(url=url_send_req, data=payload_send_req, headers=headers_send_req)
+        if r_send_req.status_code == 200:
+            r_send_req = r_send_req.json()
+        else:
+            print(r_send_req.text)
+    except Exception as errh:
+        print(errh)
+
+    return r_send_req
+
+if __name__ == "__main__":
+    # agent_did = create_public_did(9001, 8001)
+    # print(agent_did)
+    # steward_agent_conn_id, agent_steward_conn_id = make_Connection(9001, 8001)
+    # print(steward_agent_conn_id, agent_steward_conn_id)
+
+    # sample_schema = {
+    #     "attributes": [
+    #         "score"
+    #     ],
+    #     "schema_name": "prefs",
+    #     "schema_version": "1.0"
+    # }
+    # json_schema = json.dumps(sample_schema)
+    # schema = gen_Schema(8001, json_schema)
+    # print(schema)
+
+    # sample_CredDef = {
+    #     "revocation_registry_size": 1000,
+    #     "schema_id": "Th7MpTaRZVRYnPiabds81Y:2:prefs:1.0",
+    #     "support_revocation": False,
+    #     "tag": "default"
+    # }
+    # json_CredDef = json.dumps(sample_CredDef)
+    # CredDef = gen_CredDef(8001, json_CredDef)
+    # print(CredDef)
+
+    #####################################################
+
+    sample_cred = {
+        "auto_remove": True,
+        "comment": "string",
+        "connection_id": "b20a154e-6c16-49ec-ba83-bac593c6a47e",
+        "credential_preview": {
+            "@type": "issue-credential/2.0/credential-preview",
+            "attributes": [
+            {
+                "mime-type": "text/plain",
+                "name": "score",
+                "value": "69"
+            }
+            ]
+        },
+        "filter": {
+            "indy": {
+            "cred_def_id": "Th7MpTaRZVRYnPiabds81Y:3:CL:14:default",
+            "issuer_did": "Th7MpTaRZVRYnPiabds81Y",
+            "schema_id": "Th7MpTaRZVRYnPiabds81Y:2:prefs:1.0",
+            "schema_issuer_did": "Th7MpTaRZVRYnPiabds81Y",
+            "schema_name": "prefs",
+            "schema_version": "1.0"
+            }
+        },
+        "trace": True
+    }
+
+    # json_Cred = json.dumps(sample_cred)
+    # Cred_resp = issue_Cred(8001, 9001, json_Cred)
+    # print(Cred_resp)
+
+    print(send_cred_req(9001, "7EosWdvAhYnzUJE2YCjiNr"))
+
+    #############################################################
