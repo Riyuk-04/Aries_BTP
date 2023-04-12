689524babbea1cee49eaa52654676f6dd84c8b45
Riyuk-04 <ishangoel04062001@gmail.com>
2023-04-12 15:19:28 +0530

    reduced-json calls in demo

--- aries_controllers/app.py
+++ aries_controllers/app.py
@@ -4,17 +4,17 @@ import json
 
 app = Flask(__name__, template_folder='templates')
 
-@app.route('/hospital')
-def hospital():
-    return render_template('hospital.html')
+@app.route('/issuer')
+def issuer():
+    return render_template('issuer.html')
 
 @app.route('/alice')
 def alice():
     return render_template('alice.html')
 
-@app.route('/verifier')
-def verifier():
-    return render_template('verifier.html')
+@app.route('/hospital')
+def hospital():
+    return render_template('hospital.html')
 
 
 @app.route('/create_public_did', methods=['POST'])
@@ -26,6 +26,8 @@ def create_public_did_endpoint():
 
 @app.route('/make_Connection', methods=['POST'])
 def make_Connection_endpoint():
+    # request = json.dumps(request)
+    # request = request.json()
     agent1_admin_port = request.json['agent1_admin_port']
     steward_admin_port = request.json['steward_admin_port']
     result = make_Connection(agent1_admin_port, steward_admin_port)
@@ -49,30 +51,79 @@ def gen_CredDef_endpoint():
 
 @app.route('/issue_Cred', methods=['POST'])
 def issue_Cred_endpoint():
-    steward_admin_port = request.json['steward_admin_port']
-    cred = request.json['cred']
+    connection_id  = request.data.decode()
+    steward_admin_port = 8001
+    cred = {
+        "auto_remove": True,
+        "comment": "string",
+        "connection_id": connection_id,
+        "credential_preview": {
+            "@type": "issue-credential/2.0/credential-preview",
+            "attributes": [
+            {
+                "mime-type": "text/plain",
+                "name": "score",
+                "value": "69"
+            }
+            ]
+        },
+        "filter": {
+            "indy": {
+            "cred_def_id": "Th7MpTaRZVRYnPiabds81Y:3:CL:15:default",
+            "issuer_did": "Th7MpTaRZVRYnPiabds81Y",
+            "schema_id": "Th7MpTaRZVRYnPiabds81Y:2:prefs:1.0",
+            "schema_issuer_did": "Th7MpTaRZVRYnPiabds81Y",
+            "schema_name": "prefs",
+            "schema_version": "1.0"
+            }
+        },
+        "trace": True
+    }
     cred = json.dumps(cred)
     result = issue_Cred(steward_admin_port, cred)
     return {'result': result}
 
 @app.route('/send_cred_req', methods=['POST'])
 def send_cred_req_endpoint():
-    agent_admin_port = request.json['agent_admin_port']
-    agent_did = request.json['agent_did']
+    agent_admin_port = 9001
+    agent_did = request.data.decode()
     result = send_cred_req(agent_admin_port, agent_did)
     return {'result': result}
 
 @app.route('/send_proof_req', methods=['POST'])
 def send_proof_req_endpoint():
-    steward_admin_port = request.json['steward_admin_port']
-    proof = request.json['proof']
+    steward_admin_port = 10001
+    connection_id = request.data.decode()
+    proof = {
+        "comment": "This is a comment about the reason for the proof",
+        "connection_id": connection_id,
+        "presentation_request": {
+            "indy": {
+            "name": "Proof of Education",
+            "version": "1.0",
+            "requested_attributes": {
+                "0_name_uuid": {
+                "name": "score",
+                "restrictions": [
+                    {
+                    "cred_def_id": "Th7MpTaRZVRYnPiabds81Y:3:CL:15:default"
+                    }
+                ]
+                }
+            },
+            "requested_predicates": {
+
+            }
+            }
+        }
+    }
     proof = json.dumps(proof)
     result = send_proof_req(steward_admin_port, proof)
     return {'result': result}
 
 @app.route('/send_presentation', methods=['POST'])
 def send_presentation_endpoint():
-    agent_admin_port = request.json['agent_admin_port']
+    agent_admin_port = 9001
     result = send_presentation(agent_admin_port)
     return {'result': result}
 
--- aries_controllers/commands.sh
+++ aries_controllers/commands.sh
@@ -1,15 +1,15 @@
 sudo docker run -itd -p 9701-9708:9701-9708 ghoshbishakh/indy_pool
 
 aca-py start -it http 0.0.0.0 8000 -ot http -e http://127.0.0.1:8000 --admin-insecure-mode --admin 0.0.0.0 8001 \
---genesis-file /home/ishan/pool1.txn --seed 000000000000000000000000Steward1 --wallet-type indy --wallet-name y7 --wallet-key y7 \
+--genesis-file /home/ishan/pool1.txn --seed 000000000000000000000000Steward1 --wallet-type indy --wallet-name u4 --wallet-key u4 \
 --log-level debug --replace-public-did --auto-provision --auto-store-credential --auto-accept-requests --auto-respond-presentation-proposal --auto-verify-presentation
 
 aca-py start -it http 0.0.0.0 9000 -ot http -e http://127.0.0.1:9000 --admin-insecure-mode --admin 0.0.0.0 9001 \
---genesis-file /home/ishan/pool1.txn --wallet-type indy --wallet-name y8 --wallet-key y8 \
+--genesis-file /home/ishan/pool1.txn --wallet-type indy --wallet-name u5 --wallet-key u5 \
 --log-level debug --replace-public-did --auto-provision --auto-store-credential --auto-accept-requests --auto-respond-presentation-proposal --auto-verify-presentation
 
 aca-py start -it http 0.0.0.0 10000 -ot http -e http://127.0.0.1:10000 --admin-insecure-mode --admin 0.0.0.0 10001 \
---genesis-file /home/ishan/pool1.txn --seed 000000000000000000000000Steward2 --wallet-type indy --wallet-name y9 --wallet-key y9 \
+--genesis-file /home/ishan/pool1.txn --seed 000000000000000000000000Steward2 --wallet-type indy --wallet-name u6 --wallet-key u6 \
 --log-level debug --replace-public-did --auto-provision --auto-store-credential --auto-accept-requests --auto-respond-presentation-proposal --auto-verify-presentation
 
 aca-py provision --wallet-type indy --seed 000000000000000000000000Steward1 -e 127.0.0.1:8000 --genesis-file /home/ishan/pool1.txn \
